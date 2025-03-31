6191886ba137b5590bdc4a1330ec9e288e8c746d
Amit <heroku467@gmail.com>
2025-03-31 20:46:41 +0530

    Create channels.py

---
+++ plugins/channels.py
@@ -0,0 +1,67 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.db import db  # Database 
+import time
+import random
+import asyncio
+from config import *
+
+# Command to add the current channel to the database
+@Client.on_message(filters.command("add") & filters.channel)
+async def add_current_channel(client, message: Message):
+
+    channel_id = message.chat.id
+    channel_name = message.chat.title
+
+    try:
+        added = await db.add_channel(channel_id, channel_name)
+        if added:
+            await message.reply(f"**Channel '{channel_name}' added! ✅**")
+        else:
+            await message.reply(f"ℹ️ Channel '{channel_name}' already exists.")
+    except Exception as e:
+        print(f"Error adding channel: {e}")
+        await message.reply("❌ Failed to add channel. Contact developer.")
+
+# Command to remove the current channel from the database
+@Client.on_message(filters.command("rem") & filters.channel)
+async def remove_current_channel(client, message: Message):
+
+    channel_id = message.chat.id
+    channel_name = message.chat.title
+
+    try:
+        if await db.is_channel_exist(channel_id):
+            await db.delete_channel(channel_id)
+            await message.reply(f"**Channel '{channel_name}' removed from my database!**")
+        else:
+            await message.reply(f"ℹ️ Channel '{channel_name}' not found.")
+    except Exception as e:
+        print(f"Error removing channel: {e}")
+        await message.reply("❌ Failed to remove channel. Try again.")
+
+# Command to list all connected channels
+@Client.on_message(filters.command("channels") & filters.private & filters.user(ADMIN))
+async def list_channels(client, message: Message):
+    try:
+        await message.react(emoji=random.choice(REACTIONS), big=True)  # React with a random emoji
+    except:
+        pass
+
+    # Retrieve all channels from the database
+    channels = await db.get_all_channels()
+
+    if not channels:
+        await message.reply("**No channels connected yet.🙁**")
+        return
+
+    total_channels = len(channels)
+
+    # Format the list of channels
+    channel_list = [f"📢 **{channel['name']}** :- `{channel['_id']}`" for channel in channels]
+    response = (
+        f"> **Total Channels :- ({total_channels})**\n\n"  # Add total count here
+        + "\n".join(channel_list)
+    )
+
+    await message.reply(response)
