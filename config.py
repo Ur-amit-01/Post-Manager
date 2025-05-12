aa81b0100371b35fb2d8ebce2c58250b0da04f90
Amit <heroku467@gmail.com>
2025-05-12 16:30:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = os.environ.get("API_ID", "22012880")
 API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8113642693:AAG9yJpZjyhKIP_nhsIIoc8ZsiTJ-gsudLU") 
 DB_NAME = os.environ.get("DB_NAME","")     
-MONGO_URI = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDcyNDUxMzAuOTI4LCJkYXRhIjp7Il9pZCI6IjYzMDZmYWFjMDNmZDg5MDAxMWVkMmEwNCIsInVzZXJuYW1lIjoiOTEwMzEzNTYyMSIsImZpcnN0TmFtZSI6IlRhd2hlZWQiLCJsYXN0TmFtZSI6IlphcmdhciIsIm9yZ2FuaXphdGlvbiI6eyJfaWQiOiI1ZWIzOTNlZTk1ZmFiNzQ2OGE3OWQxODkiLCJ3ZWJzaXRlIjoicGh5c2ljc3dhbGxhaC5jb20iLCJuYW1lIjoiUGh5c2ljc3dhbGxhaCJ9LCJlbWFpbCI6InR3aGR6YXJnYXI3ODZAZ21haWwuY29tIiwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzQ2NjQwMzMwfQ.kq6J1gBRY3BxH8zDCH0y-kTzUlE1bKu3ByQrylBXKCg"
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7150972327').split()]
 PORT = os.environ.get("PORT", "8080")
