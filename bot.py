db24a9128d683a7eef9c1d3774e589a84c22a4b5
Amit <heroku467@gmail.com>
2025-07-05 17:43:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from config import *
 from aiohttp import web
 from plugins.Post.Posting import restore_pending_deletions
 
+AMIT = os.environ.get("AMIT", "2031106491")
+
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -21,23 +23,31 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
-        self.admin_panel = None  # Initialize as None first
+        self.admin_panel = None  # Initialize as None first  
 
-    async def start(self):
+	async def start(self):
         await super().start()
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
-        
-        # Restore pending deletions using your existing function
+
+        # Restore pending deletions
         await restore_pending_deletions(self)
-        
+
+        # Logging
         logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
         logging.info(f"{me.first_name} Pending deletions restored successfully.")
 
-    async def stop(self, *args):
+        # Notify admins
+        for admin_id in AMIT:
+            try:
+                await self.send_message(admin_id, f"f"**⚡Back online, baby 💋\nDon’t even think about using another bot... I'm all yours! 😤❤️**"")
+            except Exception as e:
+                logging.warning(f"Failed to send restart notification to {admin_id}: {e}")
+
+	async def stop(self, *args):
         await super().stop()      
         logging.info("Bot Stopped 🙄")
-    
+
 bot = Bot()
 bot.run()
